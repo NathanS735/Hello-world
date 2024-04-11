4e33e5b6473efe1b3452422ee1d11518e452d627
NathanS735 <166425151+NathanS735@users.noreply.github.com>
2024-04-11 09:49:06 -0600

    Update hello-world.java

--- hello-world.java
+++ hello-world.java
@@ -1,8 +1,7 @@
-#!/usr/bin/java
-public class Example {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World");
-
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
     }
-}
+};
